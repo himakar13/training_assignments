60069968dfc723d0286733736a54eb905358f112
himakar13 <103922585+himakar13@users.noreply.github.com>
2022-05-05 10:07:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_based_test_1
+{
+    class Passanger
+    {
+        public string name;
+        public string age;
+        public string date_of_travel;
+        public string no_of_tickets;
+
+        public void passanger(string username, string age_of_passanger, string date, string tickets)
+        {
+            name = username;
+            age = age_of_passanger;
+            date_of_travel = date;
+            no_of_tickets = tickets;
+        }
+
+        public static void Ticketbooking()
+        {
+            int no_of_tickets = 0;
+
+            if (no_of_tickets > 5)
+            {
+                Console.WriteLine("can not book than 5 tickets at a time");
+            }
+            else
+            {
+                Console.WriteLine("ticket bookedsuccessfully");
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
